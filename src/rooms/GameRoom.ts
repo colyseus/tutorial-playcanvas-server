c38e118c657e2fa08ffa1fc7ce2a65425bfba84d
Lahiru Pathirage <lpsandaruwan@gmail.com>
2022-01-10 15:37:55 +0530

    Update GameRoom.ts
    Player position randomized.

--- src/rooms/GameRoom.ts
+++ src/rooms/GameRoom.ts
@@ -3,7 +3,7 @@ import {GameState, Player} from "./schema/GameState";
 
 export class GameRoom extends Room<GameState> {
 
-    maxClients = 2;
+    maxClients = 5;
 
     onCreate(options: any) {
         this.setState(new GameState());
@@ -11,24 +11,20 @@ export class GameRoom extends Room<GameState> {
         this.onMessage("updatePosition", (client, data) => {
             console.log("update received -> ");
             console.debug(JSON.stringify(data));
-            const player = this.state.players.get(data["id"]);
+            const player = this.state.players.get(client.sessionId);
             player.x = data["x"];
             player.y = data['y'];
             player.z = data["z"];
+            this.state.players.set(client.sessionId, player);
         });
     }
 
     onJoin(client: Client, options: any) {
+        // Randomize player position on initializing.
         const newPlayer = new Player();
-        if (this.state.players.size == 1) {
-            newPlayer.x = 1;
-            newPlayer.y = 1.031;
-            newPlayer.z = 0;
-        } else {
-            newPlayer.x = 0;
-            newPlayer.y = 1.031;
-            newPlayer.z = 0
-        }
+        newPlayer.x = Math.random() * 7.2 - 3.6;
+        newPlayer.y = 1.031;
+        newPlayer.z = Math.random() * 7.2 - 3.6;
         this.state.players.set(client.sessionId, newPlayer);
         console.log(client.sessionId, "joined!");
     }
